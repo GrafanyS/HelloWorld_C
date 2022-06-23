44add74f14dbd88cd6d87c16ac7db41ef5613740
GrafanyS <admin@valet.of.by>
2022-06-23 14:50:44 +0300

    git commit c желтой ошибкой

--- example008Fun/Program.cs
+++ example008Fun/Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿void FillArray(int[] collecion)
+
+{
+    int length = collecion.Length;
+    int index = 0;
+    while (index <length)
+    {
+        collecion[index] = new Random().Next(1, 10);
+        index++;
+    };
+}
+
+void PrintArray(int[] col)
+
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+    
+    
+}
+
+int Inex0f(int[] collecion, int find)
+{
+    int count = collecion.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collecion[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos =Inex0f(array, 4);
+Console.WriteLine(pos);
