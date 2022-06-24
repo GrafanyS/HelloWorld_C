b550751fdb44141ca66a97b7a973e9cc49a5e1d3
GrafanyS <admin@valet.of.by>
2022-06-24 20:02:28 +0300

    дополнения

--- example005Con/Program.cs
+++ example005Con/Program.cs
@@ -1,12 +1,12 @@
 ﻿Console.Write("Введите имя пользоваткеля:");
 string username = Console.ReadLine();
-if (!String.IsNullOrEmpty(username))
-if (username.ToLower() == "маша")
-{
-    Console.WriteLine("Ура, это же МАША!");
-}
-else
-{
-    Console.Write("Привет, ");
-    Console.WriteLine(username);
-}
+if (!String.IsNullOrWhiteSpace(username))
+    if (username.ToLower() == "маша")
+    {
+        Console.WriteLine("Ура, это же МАША!");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
