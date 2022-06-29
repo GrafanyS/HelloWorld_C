7e0729d67c591e7f9b7f9b26bec8956bd7fc4757
GrafanyS <admin@valet.of.by>
2022-06-29 22:00:07 +0300

    исправил код больше предупреждение не мешает

--- example005Con/Program.cs
+++ example005Con/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.Write("Введите имя пользоваткеля:");
-string username = Console.ReadLine();
+string? username = Console.ReadLine();
 if (!String.IsNullOrWhiteSpace(username))
     if (username.ToLower() == "маша")
     {
